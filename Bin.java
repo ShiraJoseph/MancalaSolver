c9f2c5863aecb8808e326b2db0798fbe8007a9ae
Shira Friedman <efriedman.contact@gmail.com>
2016-05-31 19:58:08 -0400

    Create Bin.java

---
+++ Bin.java
@@ -0,0 +1,18 @@
+package mancalaPackage;
+
+public class Bin {
+	private int index;
+	private int stones;
+	public int getStones() {
+		return stones;
+	}
+	public void setStones(int stones) {
+		this.stones = stones;
+	}
+	public int getIndex() {
+		return index;
+	}
+	public void setIndex(int index) {
+		this.index = index;
+	}
+}
