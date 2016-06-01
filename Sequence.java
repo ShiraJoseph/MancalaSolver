c9d888f6772a7690b08e45e177f2732405f49d2e
Shira Friedman <efriedman.contact@gmail.com>
2016-06-01 18:07:40 -0400

    Update Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -2,29 +2,8 @@ package mancalaPackage;
 
 import java.util.ArrayList;
 
-public class Sequence {
-	private static ArrayList<Integer> record = new ArrayList<Integer>();
-	private String layout;
-	private int potTotal;
-	
-	public static ArrayList<Integer> getRecord() {
-		return record;
-	}
-	public static void setRecord(ArrayList<Integer> record) {
-		Sequence.record = record;
-	}
-	public String getLayout() {
-		return layout;
-	}
-	public void setLayout(String layout) {
-		this.layout = layout;
-	}
-	public int getPotTotal() {
-		return potTotal;
-	}
-	public void setPotTotal(int potTotal) {
-		this.potTotal = potTotal;
-	}
-	
-	
+public class Sequence {	
+	public ArrayList<Integer> turns = new ArrayList<Integer>();
+	public ArrayList<String> layouts = new ArrayList<String>();//refers to the layout BEFORE the corresponding move is made
+	public int potTotal;
 }
