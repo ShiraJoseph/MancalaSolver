6a187dd35823d47c1c0dc2f1b46c2f6d34609a34
Shira Friedman <efriedman.contact@gmail.com>
2016-06-01 18:07:10 -0400

    Update Bin.java

--- Bin.java
+++ Bin.java
@@ -1,18 +1,7 @@
 package mancalaPackage;
 
 public class Bin {
-	private int index;
-	private int stones;
-	public int getStones() {
-		return stones;
-	}
-	public void setStones(int stones) {
-		this.stones = stones;
-	}
-	public int getIndex() {
-		return index;
-	}
-	public void setIndex(int index) {
-		this.index = index;
-	}
+	public int index;
+	public int stones;
+	public boolean isEmpty() {return (this.stones == 0) ? true : false;}
 }
