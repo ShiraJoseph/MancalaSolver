3e118e555568c2e2cc807e8ce130044ff5d62157
Shira Friedman <efriedman.contact@gmail.com>
2016-06-01 18:05:48 -0400

    Update MancalaSolver.java
    First draft

--- MancalaSolver.java
+++ MancalaSolver.java
@@ -3,57 +3,92 @@ package mancalaPackage;
 import java.util.ArrayList;
 
 public class MancalaSolver {
-	public static int startingBin=0;
+	
 	public static Bin[] bins = new Bin[13];
+	public static Bin startingBin=new Bin();
 	public static ArrayList<Sequence> sequences = new ArrayList<Sequence>();
-
+	public static Sequence currSequence=new Sequence();
+	public static Boolean nextBinExists=false;
+	
 	public static void main(String[] args) {		  
-		    setLayout("4 4 4 4 4 4 0 4 4 4 4 4 4");
-		    findStartingBin(0);
-		    moveStones();
+		sequences.add(currSequence);
+		currSequence.layouts.add("4 4 4 4 4 4 0 4 4 4 4 4 4");
+		layoutStones(currSequence.layouts.get(0));
+		findStartingBin(0);
+		moveStones();
 	}
 		  
-	public static void setLayout(String layout){
+	public static void layoutStones(String layout){
 		String[] stoneAmount=layout.split(" ");	
-		for(int i=0;i<13;i++){
-				bins[i].setStones(Integer.valueOf(stoneAmount[i]));				
+		for(int i=0;i<=12;i++){
+				bins[i].stones=Integer.valueOf(stoneAmount[i]);				
 		}
 	}
 		  
-	public static Boolean findStartingBin(int start){
-//		    Find the first bin from start that isn't empty and write down its index on the current line and make it the starting bin. return true
-//		    if don't find non-empty bin before pot, 
-				return false;
+	public static void findStartingBin(int start){
+		for(int i=start;i<6;i++){
+			if (!bins[i].isEmpty()){
+				startingBin=bins[i];
+				currSequence.turns.add(i); 
+			nextBinExists= true;
+			}
+		}
+		nextBinExists=false;
 	}
 		  
 	public static void moveStones(){
-//		    Pick up the stones from starting bin. 
-//		    Put 1 stone in the next bin, 1 in the bin after that, etcetera until hand is empty. That last bin becomes the starting bin
-		    
-//		    if (starting bin is the pot) {
-//		      write down the layout below the last index of the sequence
-//		      findStartingBin(0);
-//		    }else if (starting bin has 1 stone){
-//		      closeSequenceAndOpenNext();
-//		    }else{
-//		      moveStones();
-//		    }
+		int currIndex=startingBin.index;
+		int hand=startingBin.stones;
+		startingBin.stones=0;
+		while(hand>0){
+			currIndex++;
+			if (currIndex==13)currIndex=0;
+			bins[currIndex].stones++;
+			hand--;
+		}
+		startingBin=bins[currIndex];
+		
+		if (currIndex==6){
+			storeLayout(currSequence);
+			findStartingBin(0);
+		}else if(startingBin.stones==1){
+			closeSequenceAndOpenNext();
+		}else{
+			moveStones();
+		}
 	}
-		  
+	
+	public static void storeLayout(Sequence sequence){
+		String layout="";
+		for(Bin bin:bins){
+			layout+=(Integer.valueOf(bin.stones)+" ");
+		}
+		sequence.layouts.add(layout.trim());
+		System.out.println(layout.trim());
+	}
+	
 	public static void closeSequenceAndOpenNext(){
-//		    remove the one stone from the startingBin and the stones from the bin opposite and add them to the pot. 
-//		    Write down the pot total for that sequence
-//		    go to the next sequence and write down the same exact sequence and layouts as the line before
+			bins[6].stones=bins[12-startingBin.index].stones + 1;
+			currSequence.potTotal=bins[6].stones;
+			
+			Sequence nextSequence=new Sequence();
+			nextSequence.layouts=currSequence.layouts;
+			nextSequence.turns=currSequence.turns;
+			currSequence=nextSequence;
+			sequences.add(currSequence);
+			
 	}
 		  
 	public static void retractMoves(){
-//		    setLayout(string of last layout recorded);
-//		    if (findStartingBin(index listed at the end of the line + 1)){
-//		      replace the last index written down with this new bin's index and assign it to startingBin 
-//		      moveStones();
-//		    }else {
-//		      remove the last index written on the line and the last layout before that 
-//		      retractMoves();
-//		    }
+			layoutStones(currSequence.layouts.get(currSequence.layouts.size()-1));
+			findStartingBin(currSequence.turns.get(currSequence.turns.size()-1) + 1);
+			currSequence.turns.remove(currSequence.turns.size()-1);
+			if (nextBinExists){
+				currSequence.turns.add(startingBin.index);
+				moveStones();
+		    }else {
+		    	currSequence.layouts.remove(currSequence.layouts.size()-1);
+		    	retractMoves();
+		    }
 	}
 }
